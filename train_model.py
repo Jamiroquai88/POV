9c6020884e55f5c00fb29370cbe022de11304074
Jan Profant <jan.profant@phonexia.com>
2018-11-09 09:14:58 +0100

    changed size

--- train_model.py
+++ train_model.py
@@ -52,12 +52,14 @@ if __name__ == '__main__':
     parser.add_argument('--imgs4validation', required=False, default=5, type=int,
                         help='number of images used for cross-validation')
     parser.add_argument('--num-persons', required=False, default=0, type=int, help='number of persons for training')
-    parser.add_argument('--no-use-gpu', required=False, default=False, action='store_true', help='use GPU for processing')
+    parser.add_argument('--no-use-gpu', required=False, default=False, action='store_true',
+                        help='use GPU for training')
     
     args = parser.parse_args()
     if args.no_use_gpu is not True:
         try:
-            command='nvidia-smi --query-gpu=memory.free,memory.total --format=csv |tail -n+2| awk \'BEGIN{FS=" "}{if ($1/$3 > 0.98) print NR-1}\''
+            command = 'nvidia-smi --query-gpu=memory.free,memory.total --format=csv |tail -n+2| ' \
+                    'awk \'BEGIN{FS=" "}{if ($1/$3 > 0.98) print NR-1}\''
             gpu_idx = subprocess.check_output(command, shell=True).rsplit(b'\n')[0].decode('utf-8')
             os.environ["CUDA_VISIBLE_DEVICES"] = gpu_idx
             logger.info('Using GPU {}.'.format(gpu_idx))
@@ -88,6 +90,7 @@ if __name__ == '__main__':
             train_data.extend([x[0] for x in persons_dict[person][args.imgs4validation:]])
             test_labels.extend([person for x in range(args.imgs4validation)])
             train_labels.extend([person for x in range(num_imgs - args.imgs4validation)])
+            num_persons += 1
 
     label_encoder = LabelEncoder().fit(test_labels)
     test_encoded_labels = label_encoder.transform(test_labels)
@@ -95,11 +98,12 @@ if __name__ == '__main__':
 
     assert len(test_encoded_labels) == len(test_data)
     assert len(train_encoded_labels) == len(train_data)
-    print(test_data[0].shape)
+    logger.info('Using {} images for training with {} classes.'.format(len(train_data), len(set(train_labels))))
+    logger.info('Using {} images for testing with {} classes.'.format(len(test_data), len(set(test_labels))))
 
-    # model = create_model(input_shape=train_data[0].shape, num_classes=len(set(test_labels)))
-    from resnet_model import resnet_v2
-    model = resnet_v2(input_shape=train_data[0].shape, depth=56, num_classes=len(set(test_labels)))
+    model = create_model(input_shape=train_data[0].shape, num_classes=len(set(test_labels)))
+    # from resnet_model import resnet_v2
+    # model = resnet_v2(input_shape=train_data[0].shape, depth=56, num_classes=len(set(test_labels)))
     batch_size = 128
     epochs = 50
     model.compile(optimizer=Adam(lr=0.00001), loss='categorical_crossentropy', metrics=['accuracy'])
