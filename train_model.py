fcc4e87e1a345f80e475edb4145383f8d2b32ef7
Jan Profant <jan.profant@phonexia.com>
2018-11-11 12:38:02 +0100

    wip

--- train_model.py
+++ train_model.py
@@ -13,10 +13,14 @@ import subprocess
 
 from keras.utils import to_categorical
 from keras.optimizers import Adam
+from keras.models import load_model, Model
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.spatial.distance import euclidean
 from sklearn.preprocessing import LabelEncoder
 
+from utils import get_eer, l2_norm, cosine_similarity
+from resnet_model import resnet_v2
 from keras_model import create_model
 
 logger = logging.getLogger()
@@ -54,9 +58,12 @@ if __name__ == '__main__':
     parser.add_argument('--imgs4validation', required=False, default=5, type=int,
                         help='number of images used for cross-validation')
     parser.add_argument('--num-persons', required=False, default=0, type=int, help='number of persons for training')
+    parser.add_argument('--num-ver-persons', required=False, default=0, type=int,
+                        help='number of persons for verification')
     parser.add_argument('--learning-rate', required=False, default=0.0001, type=float, help='learning rate')
     parser.add_argument('--no-use-gpu', required=False, default=False, action='store_true',
                         help='use GPU for training')
+    parser.add_argument('--no-train-model', required=False, default=False, action='store_true')
     
     args = parser.parse_args()
     if args.no_use_gpu is not True:
@@ -82,51 +89,86 @@ if __name__ == '__main__':
                     continue
 
     train_data, test_data, train_labels, test_labels, num_persons = [], [], [], [], 0
+    ver_test_data, ver_test_labels, num_ver_persons = [], [], 0
     for person in persons_dict:
         num_imgs = len(persons_dict[person])
         if num_imgs > 3 * args.imgs4validation:
             if args.num_persons != 0:
                 if num_persons > args.num_persons:
-                    break
+                    continue
             np.random.shuffle(persons_dict[person])
             test_data.extend([x[0] for x in persons_dict[person][:args.imgs4validation]])
             train_data.extend([x[0] for x in persons_dict[person][args.imgs4validation:]])
             test_labels.extend([person for x in range(args.imgs4validation)])
             train_labels.extend([person for x in range(num_imgs - args.imgs4validation)])
             num_persons += 1
+        else:
+            # we are gonna use this data for face verification
+            if args.num_ver_persons != 0:
+                if num_ver_persons == args.num_ver_persons:
+                    continue
+            ver_test_data.extend([x[0] for x in persons_dict[person]])
+            ver_test_labels.extend([person for x in range(num_imgs)])
+            num_ver_persons += 1
 
-    label_encoder = LabelEncoder().fit(test_labels)
-    test_encoded_labels = label_encoder.transform(test_labels)
-    train_encoded_labels = label_encoder.transform(train_labels)
+    if args.no_train_model is not True:
+        label_encoder = LabelEncoder().fit(test_labels)
+        test_encoded_labels = label_encoder.transform(test_labels)
+        train_encoded_labels = label_encoder.transform(train_labels)
 
-    assert len(test_encoded_labels) == len(test_data)
-    assert len(train_encoded_labels) == len(train_data)
-    logger.info('Using {} images for training with {} classes.'.format(len(train_data), len(set(train_labels))))
-    logger.info('Using {} images for testing with {} classes.'.format(len(test_data), len(set(test_labels))))
+        assert len(test_encoded_labels) == len(test_data)
+        assert len(train_encoded_labels) == len(train_data)
+        logger.info('Using {} images for training with {} classes.'.format(len(train_data), len(set(train_labels))))
+        logger.info('Using {} images for testing with {} classes.'.format(len(test_data), len(set(test_labels))))
 
-    #model = create_model(input_shape=train_data[0].shape, num_classes=len(set(test_labels)))
-    from resnet_model import resnet_v2
-    model = resnet_v2(input_shape=train_data[0].shape, depth=20, num_classes=len(set(test_labels)))
-    batch_size = 128
-    epochs = 50
-    lr = args.learning_rate
-    logger.info('Using batch size: {}, number of epochs: {}, learning rate: {}.'.format(batch_size, epochs, lr))
-    # old lr 0.00001
-    model.compile(optimizer=Adam(lr=lr), loss='categorical_crossentropy', metrics=['accuracy'])
+        # choose model
+        # model = create_model(input_shape=train_data[0].shape, num_classes=len(set(test_labels)))
+        model = resnet_v2(input_shape=train_data[0].shape, depth=20, num_classes=len(set(test_labels)))
 
-    model.summary()
-    train_data = np.array(train_data)
-    test_data = np.array(test_data)
-    train_encoded_labels = to_categorical(train_encoded_labels)
-    test_encoded_labels = to_categorical(test_encoded_labels)
-    print(train_data.shape)
-    print(train_encoded_labels.shape)
-    history = model.fit(train_data, train_encoded_labels, batch_size=batch_size, epochs=epochs,
-                        verbose=1, validation_data=(test_data, test_encoded_labels))
+        batch_size = 128
+        epochs = 50
+        lr = args.learning_rate
+        logger.info('Using batch size: {}, number of epochs: {}, learning rate: {}.'.format(batch_size, epochs, lr))
+        model.compile(optimizer=Adam(lr=lr), loss='categorical_crossentropy', metrics=['accuracy'])
 
-    model.evaluate(test_data, test_encoded_labels)
-    plot_curves()
-    model.save('model.h5')
+        model.summary()
+        train_data = np.array(train_data)
+        test_data = np.array(test_data)
+        train_encoded_labels = to_categorical(train_encoded_labels)
+        test_encoded_labels = to_categorical(test_encoded_labels)
+        history = model.fit(train_data, train_encoded_labels, batch_size=batch_size, epochs=epochs,
+                            verbose=1, validation_data=(test_data, test_encoded_labels))
+
+        model.evaluate(test_data, test_encoded_labels)
+        plot_curves()
+        model.save('model.h5')
+    else:
+        model = load_model('model.h5')
+        model.summary()
+
+        ver_num_persons, ver_num_faces = len(set(ver_test_labels)), len(ver_test_labels)
+        logger.info('Using {} persons with {} images for verification.'.format(ver_num_persons, ver_num_faces))
+
+        ver_test_data = np.array(ver_test_data)
+        # print(model.predict(np.array(ver_test_data[:2])).shape)
+
+        output_layer_model = Model(inputs=model.input, outputs=model.get_layer('flatten_1').output)
+        face_prints = l2_norm(output_layer_model.predict(np.array(ver_test_data)))
+        print(face_prints[0])
+        print(face_prints.shape)
+        assert ver_num_faces == face_prints.shape[0]
+
+        tar_scores, non_scores = [], []
+        for x in range(ver_num_faces):
+            for y in range(x):
+                distance = cosine_similarity(face_prints[x], face_prints[y])
+                print(x, y, distance)
+                if ver_test_labels[x] == ver_test_labels[y]:
+                    tar_scores.append(distance)
+                else:
+                    non_scores.append(distance)
+
+        print('EER: {}'.format(get_eer(tar_scores, non_scores)))
 
 
 
---
+++ utils.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2018 Phonexia
+# Author: Jan Profant <jan.profant@phonexia.com>
+# All Rights Reserved
+
+import numpy as np
+from scipy import spatial
+
+
+def get_eer(tar, non):
+    """ Obtain Equal Error Rate.
+
+    Args:
+        tar (np.array|List[float]): target scores as 1 dimensional vector
+        non (np.array|List[float]): non-target scores as 1 dimensional vector
+
+    Returns:
+        tuple: EER, EER threshold, EER misses, EER false alarms
+    """
+    target_scores = sorted(np.array(tar).flatten())
+    imposter_scores = sorted(np.array(non).flatten())
+    min_tar_score, max_imp_score = min(target_scores), max(imposter_scores)
+    # system did not make any mistake, EER algorithm would return bullsense
+    # return all zeros and calculate the threshold as unweighted average of min target score and max impostor score
+    if max_imp_score < min_tar_score:
+        return 0.0, (max_imp_score + min_tar_score) / 2, 0, 0
+    target_count, imposter_count = len(target_scores), len(imposter_scores)
+    eer = 0.0
+    eer_dif = float('inf')
+    imp_index = 0
+    tar_index = 0
+    eer_thr = None
+    eer_miss, eer_fa = None, None
+    if target_scores:
+        threshold = float(target_scores[0])
+    elif imposter_scores:
+        threshold = float(imposter_scores[0])
+    else:
+        raise ValueError('Cannot evaluate two empty lists.')
+
+    while True:
+        while imp_index < imposter_count and threshold >= imposter_scores[imp_index]:
+            imp_index = imp_index + 1
+        while tar_index < target_count and threshold >= target_scores[tar_index]:
+            tar_index = tar_index + 1
+        miss = tar_index
+        fa = imposter_count - imp_index
+        if target_count > 0:
+            frr = 1.0 * miss / target_count if target_count else 0.0
+        else:
+            frr = 0.0
+        far = 1.0 * fa / imposter_count if imposter_count else 0.0
+        if abs(frr - far) < eer_dif:
+            eer_dif = abs(frr - far)
+            eer = (frr + far) / 2 * 100
+            eer_thr = threshold
+            eer_miss, eer_fa = miss, fa
+        if imp_index < imposter_count and tar_index < target_count:
+            threshold = float(min(imposter_scores[imp_index], target_scores[tar_index]))
+        else:
+            break
+    if eer_thr is not None and not isinstance(eer_thr, float):
+        raise ValueError('Invalid threshold value %s (type %s), float or None expected.' % (eer_thr, type(eer_thr)))
+    if not isinstance(eer, float):
+        raise ValueError('Invalid threshold value %s (type %s), float expected.' % (eer, type(eer)))
+    return eer, eer_thr, eer_miss, eer_fa
+
+
+def l2_norm(vec_or_matrix):
+    """ L2 normalization of vector array.
+
+    Args:
+        vec_or_matrix (np.array): one vector or array of vectors
+
+    Returns:
+        np.array: normalized vector or array of normalized vectors
+    """
+    if len(vec_or_matrix.shape) == 1:
+        # linear vector
+        return vec_or_matrix / np.linalg.norm(vec_or_matrix)
+    elif len(vec_or_matrix.shape) == 2:
+        return vec_or_matrix / np.linalg.norm(vec_or_matrix, axis=1, ord=2)[:, np.newaxis]
+    else:
+        raise ValueError('Wrong number of dimensions, 1 or 2 is supported, not %i.' % len(vec_or_matrix.shape))
+
+
+def cosine_similarity(v1, v2):
+    """Compute similarity distance of v1 to v2: (v1 dot v2)/(||v1||*||v2||).
+
+    Args:
+        v1 (np.array): first vector
+        v2 (np.array): second vector
+
+    Returns:
+        float: cosine similarity
+    """
+    return 1 - spatial.distance.cosine(v1, v2)
